c37a73c59bbb048737c1754299b006739eac140d
Minimalist <kaabakedvonze@gmail.com>
2025-04-04 15:23:30 +0400

    [feat] Clean solution

--- CleanSolution.sh
+++ CleanSolution.sh
@@ -2,6 +2,7 @@
 
 find . -name "*_linux64.mak" -exec rm {} \;
 find . -name "*_linux32.mak" -exec rm {} \;
+find . -name "*_srv.mak" -exec rm {} \;
 find . -name "*.vpc_crc" -exec rm {} \;
 find . -name "*.vpc_cache" -exec rm {} \;
 rm csgo_partner.mak
